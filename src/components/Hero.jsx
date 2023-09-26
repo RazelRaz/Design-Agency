68dcb20c21b3e5d53c0fdce3599d999f8ef1546a
RazelRaz <razel16dcm@gmail.com>
2023-09-26 16:46:38 +0600

    navbar and hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import styles from './components.module.css'
+import Image from 'next/image';
+import HeroOne from '../../public/images/hero1.png'
+import HeroTwo from '../../public/images/hero2.png'
+import HeroThree from '../../public/images/hero3.png'
+import HeroFour from '../../public/images/hero4.png'
+import Link from 'next/link';
 
 const Hero = () => {
     return (
         <div className={styles.heroArea}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <div className={styles.heroAreaLeft}></div>
-                    <div className={styles.heroAreaRight}></div>
+                    <div className={styles.heroAreaLeft}>
+                        <h1>Increase Your Customers Loyalty and Satisfaction</h1>
+                        <p>We help businesses like yours earn more customers, standout from competitors, make more money</p>
+                        <div className={styles.heroBtn}>
+                            <Link href={'/services'}>Get Started</Link>
+                        </div>
+                    </div>
+                    <div className={styles.heroAreaRight}>
+                        <div className={styles.heroAreaRightTop}>
+                            <Image src={HeroOne}  alt="Hero" />
+                            <Image src={HeroTwo}  alt="Hero" />
+                        </div>
+                        <div className={styles.heroAreaRightBottom}>
+                            <Image src={HeroThree}  alt="Hero" />
+                            <Image src={HeroFour}  alt="Hero" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -4,30 +4,44 @@ import Image from 'next/image';
 import LogoImg from '../../public/images/DesignAGENCY.png'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link';
-
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 const SiteNavbar = () => {
 
     let currentPath = usePathname();
+    const [isNavShowing, setIsNavSHowing] = useState(false)
 
     return (
         <nav className={styles.navArea}>
             <div className={styles.container}>
                 <div className={styles.content}>
                     <div className={styles.navAreaLogo}>
-                        <Image src={LogoImg}  alt="Logo" />
+                        <Link href={'/'}><Image src={LogoImg}  alt="Logo" /></Link>
+                        
                     </div>
                     <div className={styles.navAreaMainMenu}>
-                        <ul>
-                            <Link className={currentPath === '/' ? 'active' : ''} href={"/"}>Home</Link>
+                        <ul className={`navLinks ${isNavShowing ? 'show_nav' : 'hide_nav'}`}>
+                            <Link  onClick={() => setIsNavSHowing(prev => !prev)} className={currentPath === '/' ? 'active' : ''} href={"/"}>Home</Link>
                             <Link className={currentPath === '/team' ? 'active' : ''} 
                             href={"/team"}>
                                 Team
                             </Link>
-                            <Link className={currentPath === '/services' ? 'active' : ''} href={"/services"}>Services</Link>
-                            <Link className={currentPath === '/projects' ? 'active' : ''} href={"/projects"}>Projects</Link>
-                            <Link className={currentPath === '/testimonials' ? 'active' : ''} href={"/testimonials"}>Testimonials</Link>
-                            <Link className={styles.menLogin} href={'#'}>Login</Link>
-                            <Link className={styles.menSign} href={'#'}>Sign Up</Link>
+                            <Link  onClick={() => setIsNavSHowing(prev => !prev)} className={currentPath === '/services' ? 'active' : ''} href={"/services"}>Services</Link>
+                            <Link  onClick={() => setIsNavSHowing(prev => !prev)} className={currentPath === '/projects' ? 'active' : ''} href={"/projects"}>Projects</Link>
+                            <Link  onClick={() => setIsNavSHowing(prev => !prev)} className={currentPath === '/testimonials' ? 'active' : ''} href={"/testimonials"}>Testimonials</Link>
+                            <Link  onClick={() => setIsNavSHowing(prev => !prev)} className={styles.menLogin} href={'#'}>Login</Link>
+                            <Link  onClick={() => setIsNavSHowing(prev => !prev)} className={styles.menSign} href={'#'}>Sign Up</Link>
+                            
+                        </ul>
+                    </div>
+                    <div className={styles.mobMenu}>
+                        <p><FaBars ></FaBars></p>
+                        <ul className={styles.mobMenuLinks}>
+                            <Link href={"/services"}>services</Link>
+                            <Link href={"/projects"}>projects</Link>
+                            <Link href={"/testimonials"}>Testimonials</Link>
+                            <Link href={"#"}>Login</Link>
+                            <Link href={"#"}>Sign Up</Link>
                         </ul>
                     </div>
                 </div>
