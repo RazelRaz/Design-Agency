bb77c5ebe7c93942e6c1a01e6cd5925c797d2985
RazelRaz <razel16dcm@gmail.com>
2023-09-27 23:49:29 +0600

    footer top added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import styles from './components.module.css'
+import Image from 'next/image';
+import FootLogo from '../../public/images/log.png'
+import Social from '../../public/images/social.png'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className={styles.footerArea}>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.footerBox}>
+                        <div className={styles.footerTop}>
+                            <div className={styles.footerLogo}>
+                                <Image src={FootLogo} alt="Logo" />
+                            </div>
+                            
+                            <p>Some footer text about the  Agency. Just a little description to help people understand you better</p>
+                            <Image src={Social} alt="icons" />
+                        </div>
+                        <div className={styles.footerMiddle}>
+                            <div className={styles.footerMiddleBox}>
+                                <h6>Quick Links</h6>
+                                <ul>
+                                    <li>Services</li>
+                                    <li>Portfolio</li>
+                                    <li>About Us </li>
+                                    <li>Contact Us</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.footerBottom}>
+                            <div className={styles.footerMiddleBox}>
+                                <h6>Address</h6>
+                                <p>Design Agency Head Office.<br />
+                                    Airport Road<br />
+                                    United Arab Emirate</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
