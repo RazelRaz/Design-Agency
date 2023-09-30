00acff877c03958632e85a6ed1cd92baa4e6e680
RazelRaz <razel16dcm@gmail.com>
2023-09-30 14:54:56 +0600

    all projects area done

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,8 +1,13 @@
+import AllProjects from "@/components/AllProjects";
+import Breadcrumb from "@/components/Breadcrumb";
+import Subscribe from "@/components/Subscribe";
 
 const Page = () => {
     return (
         <div>
-            
+            <Breadcrumb title="All Projects" href="/projects"></Breadcrumb>
+            <AllProjects></AllProjects>
+            <Subscribe></Subscribe>
         </div>
     );
 };
--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import styles from './components.module.css'
+import Image from 'next/image';
+import AllPorject from '../../public/images/allproject.png'
 
 const AllProjects = () => {
     return (
-        <div>
-            <h1>AllProjects</h1>
+        <div className={styles.allProjectsArea}>
+            <div className={styles.container}>
+                <div className={styles.themeTitleBox}>
+                    <h3>All Projects</h3>
+                    <h2>Better Agency/SEO Solution At Your Fingertips</h2>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.servicesWrapper}>
+                        
+                        <div className={styles.servicesSingle}>
+                            
+                            <div className={styles.servicesSingleImageBox}>
+                                <Image src={AllPorject}  alt="All Porject" />
+                            </div>
+                            <div className={styles.servicesSingleBox}>
+                                <h2>Build & Launch without problems</h2>
+                            </div>
+
+                        </div>
+
+                        <div className={styles.servicesSingle}>
+                            
+                            <div className={styles.servicesSingleImageBox}>
+                                <Image src={AllPorject}  alt="All Porject" />
+                            </div>
+                            <div className={styles.servicesSingleBox}>
+                                <h2>Build & Launch without problems</h2>
+                            </div>
+
+                        </div>
+
+                        <div className={styles.servicesSingle}>
+                            
+                            <div className={styles.servicesSingleImageBox}>
+                                <Image src={AllPorject}  alt="All Porject" />
+                            </div>
+                            <div className={styles.servicesSingleBox}>
+                                <h2>Build & Launch without problems</h2>
+                            </div>
+
+                        </div>
+
+                        <div className={styles.servicesSingle}>
+                            
+                            <div className={styles.servicesSingleImageBox}>
+                                <Image src={AllPorject}  alt="All Porject" />
+                            </div>
+                            <div className={styles.servicesSingleBox}>
+                                <h2>Build & Launch without problems</h2>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
